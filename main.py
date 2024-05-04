5dd63c0810166d4af5516e5e35532d2b8ea657a7
Nicolas Benatti <nicolas.benatti@mail.polimi.it>
2024-05-04 10:50:56 +0200

    Added path parameter for dumptable

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from influxdb import InfluxDBClient
+import db.dbconnect as dbc
+
+DB_HOST = "84.247.188.251"
+DB_PORT = "8086"
+DB_USR = "usr"
+DB_PWD = "43238c95fd3e9450"
+
+db = dbc.DBConnector.connect(DB_HOST, DB_PORT, DB_USR, DB_PWD)
+app = FastAPI()
+
+@app.get("/dumptable/{tablename}")
+async def dump_table(tablename):
+    return db.dumpTable(tablename)
